a75c6f73327bc512e5ba4521044c62008c4cfcf1
akhil sarwar <akhilsarwar24@gmail.com>
2020-12-31 15:06:10 +0530

    commit-3

--- dijkstra.py
+++ dijkstra.py
@@ -1,86 +1,9 @@
 import math
-
-#Undirected graph
-class Graph:
-	def __init__(self):
-		self.adj_matrix = []
-		self.edges = []
-		self.nodes = []
-		self.node_num = 0
-		self.edge_num = 0
-
-
-	def add_edge(self, node1_index, node2_index, edge_weight):
-
-		node1 = self.nodes[node1_index]
-		node2 = self.nodes[node2_index]
-		self.edges.append(Edge(edge_weight, node1, node2))
-
-		self.adj_matrix[node1_index][node2_index] = 1
-		self.adj_matrix[node2_index][node1_index] = 1
-
-		self.edge_num+=1
-
-
-	def add_node(self, node_data):
-
-		self.nodes.append(Node(self.node_num, node_data))
-
-		for each in self.adj_matrix:
-			each.append(0)
-
-		new_list = []
-		for i in range(self.node_num+1):
-			new_list.append(0)
-		self.adj_matrix.append(new_list)
-
-		self.node_num+=1
-		
-
-	def find_edge(self, node1, node2):
-		for edge in self.edges:
-			if (edge.start_node.node_index == node1.node_index and edge.end_node.node_index == node2.node_index) or (edge.start_node.node_index == node2.node_index and edge.end_node.node_index == node1.node_index):
-				return edge
-
-
-
-	def print_adj_matrix(self):
-		for each in self.adj_matrix:
-			print(f'{each}')
-
-	def print_all_nodes(self):
-		for node in self.nodes:
-			node.Print()
-
-	def print_all_edges(self):
-		for edge in self.edges:
-			edge.Print()
-
-
-class Node:
-	def __init__(self, index, dist):
-		self.node_index = index
-		self.node_dist = dist
-
-	def Print(self):
-		print(f'node {self.node_index} : data : {self.node_dist}')
-
-
-class Edge:
-	def __init__(self, weight, node_start, node_end):
-		self.edge_weight = weight
-		self.start_node = node_start
-		self.end_node = node_end
-
-	def Print(self):
-		print(f'edge connecting {self.start_node.node_index} and {self.end_node.node_index}; data: {self.edge_weight}')
-
-
+from graph import *
 
 #DIJKSTRA ALGO
-def dijkstra(graph, root_index):
-	spt_set = []
-	node_list = []
+def dijkstra(graph, root_index, spt_set, node_list):
+	
 
 	for node in graph.nodes:
 		node_list.append(node)
@@ -137,15 +60,6 @@ def main():
 				data_list[i] = int(data_list[i])
 			assert(data_list[0]<node_cnt and data_list[1]<node_cnt)
 			graph.add_edge(data_list[0], data_list[1], data_list[2])
-
-
-
-	# for i in range(edge):
-	# 	x, y = input(f'edge {i}: ').split(sep=' ')
-	# 	x, y = int(x), int(y)
-	# 	assert(x<node_cnt and y<node_cnt)
-	# 	val = int(input(f'enter the {i}th edge val: '))
-	# 	graph.add_edge(x, y, val)
 
 
 	graph.print_all_nodes()
--- main.py
+++ main.py
@@ -27,12 +27,11 @@ class Grid:
 		self.edges = []
 		self.edge_count = self.graph.edge_num
 		self.grid_coordinates = func.create_grid_coordinates(self.order)
-
-
+	
 		
 		for i in range(self.node_count):
 			coordinate = func.get_cordinate(self.grid_coordinates)
-			self.nodes.append(Node_(self.graph.nodes[i] ,round(self.cell_width/4), (round(((2*coordinate[1]+1)/2)*self.cell_width), round(((2*coordinate[0]+1)/2)*self.cell_height)), (0, 255, 0), i))
+			self.nodes.append(Node_(self.graph.nodes[i] ,round(self.cell_width/4), (round(((2*coordinate[1]+1)/2)*self.cell_width), round(((2*coordinate[0]+1)/2)*self.cell_height)), (0, 255, 0), i, False))
 
 		for i in range(self.edge_count):
 			edge = self.graph.edges[i]
@@ -51,12 +50,18 @@ class Grid:
 			edge.draw(window)
 
 
+	def find_active_node(self):
+		mouse_x, mouse_y = pg.mouse.get_pos()
+		for node in self.nodes:
+			if (mouse_x > node.position[0]-node.radius and mouse_x < node.position[0]+node.radius) and (mouse_y > node.position[1]-node.radius and mouse_y < node.position[1]+node.radius):
+				return node
+
 
 
 
 
 class Node_:
-	def __init__(self, node, radius, position, color, index):
+	def __init__(self, node, radius, position, color, index, is_source_dest):
 		self.radius = radius
 		self.node = node
 		self.position = position
@@ -64,12 +69,22 @@ class Node_:
 		self.index = index
 		self.fontColor = (0, 0, 255)
 		self.Font1 = pg.font.SysFont('impact', self.radius)
+		self.is_source_dest = is_source_dest
 
 	def draw(self, window):
 		pg.draw.circle(window, self.color, self.position, self.radius)
 		txt1 = self.Font1.render(f'{self.index}', True, self.fontColor)
 		window.blit(txt1, (self.position[0]-txt1.get_width()/2, self.position[1]-txt1.get_height()/2))
 
+		mouse_x, mouse_y = pg.mouse.get_pos()
+		if (mouse_x > self.position[0]-self.radius and mouse_x < self.position[0]+self.radius) and (mouse_y > self.position[1]-self.radius and mouse_y < self.position[1]+self.radius) or self.is_source_dest:
+			# pg.draw.circle(window, [0, 0, 255], self.position, self.radius, 2)
+			self.color = (255, 255, 255)
+		else:
+			self.color = (0, 255, 0)
+
+	def make_source_dest(self):
+		self.is_source_dest = True
 
 
 
@@ -86,6 +101,8 @@ class Edge_:
 		pg.draw.line(window, self.color[0], self.start_coordinate, self.stop_coordinate, 1)
 		pg.draw.circle(window, self.color[1], self.stop_coordinate, self.radius, 3)
 
+
+def draw_process(window, node_list, spt_set):
 	
 
 
@@ -122,7 +139,13 @@ def main():
 
 		grid = Grid(graph)
 
+		_selected = False
 		run = True
+		source_dest = []
+
+		spt_set = []
+		node_list = []
+
 		while run:
 
 			WINDOW.fill((0,0,0))
@@ -131,8 +154,20 @@ def main():
 				if event.type == pg.QUIT:
 					run = false 
 					pg.quit()
+				
+				if event.type == pg.MOUSEBUTTONDOWN and not _selected:
+					active_node = grid.find_active_node()
+					if len(source_dest) < 2:
+						source_dest.append(active_node)
+						active_node.make_source_dest()
+					else:
+						_selected = True
 
-			grid.draw(WINDOW)
+
+			if _selected == False:
+				grid.draw(WINDOW)
+			else:
+				draw_process(WINDOW, node_list, spt_set)
 
 			pg.display.update()
 
